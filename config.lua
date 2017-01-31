c54b493a69e7ab7a85a7c4c205cfffcfcc50cf43
Alireza5928 <alirezaabedzahe@gmail.com>
2017-01-31 11:55:54 +0330

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,9 +1,9 @@
-bot_token = "245451534:AAEgrx-zGY_wRWkR0qYHi2xjSNL-qEjMZFU"
+bot_token = "323116787:AAEgT0pkF9I3_Ja2p7wBN_l9nWxDKn-9YLU"
 send_api = "https://api.telegram.org/bot"..bot_token
 bot_version = "6.0"
-sudo_name = "Engineer Shayan Ahmadi"
-sudo_id = 259300954
-admingp = -135847900
-sudo_num = "989351372038"
-sudo_user = "shayan_soft"
-sudo_ch = "UmbrellaTeam"
+sudo_name = "ALIREZA ABEDZADEH"
+sudo_id = 119707014
+admingp = -1106368190
+sudo_num = "989226773671"
+sudo_user = "alireza_abedzadeh"
+sudo_ch = "botantispam1"
